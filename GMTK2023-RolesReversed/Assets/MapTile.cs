3e9044f007b12f88f483b9d2665e107092fc3765
fae.exe <fae.exe@protonmail.com>
2023-07-09 14:10:00 +0200

    feat: Tile feedbacks works

--- GMTK2023-RolesReversed/Assets/MapTile.cs
+++ GMTK2023-RolesReversed/Assets/MapTile.cs
@@ -18,8 +18,28 @@ public class MapTile : MonoBehaviour
     [SerializeField] private int _tilesSpriteNumber, _tileDecorationNumber;
     [SerializeField] private bool _isVisible, _newState, _hasDecoration;
 
-    [SerializeField] private MMF_Player _turnVisible, _turnVisibleLogic, _turnNormal;
+    [SerializeField] private MMF_Player _turnVisible, _turnVisibleLogic, _turnNormal, _decorationTurnNormal, _decorationTurnSeen;
 
+    private void OnEnable()
+    {
+        Subscribe();
+    }
+
+    private void OnDisable()
+    {
+        Unsubscribe();
+    }
+
+    private void Subscribe()
+    {
+        Unsubscribe();
+        GameManager.OnGameStateChanged += OnPlayerTurn;
+    }
+
+    private void Unsubscribe()
+    {
+        GameManager.OnGameStateChanged -= OnPlayerTurn;
+    }
 
     public void Initialize(int row)
     {
@@ -28,29 +48,37 @@ public class MapTile : MonoBehaviour
         if (decorationRNG <= _decorationChance)
         {
             _hasDecoration = true;
+            
             _tileDecorationRenderer.gameObject.SetActive(true);
+            _tileDecorationHighlitRenderer.gameObject.SetActive(true);
+
             _tileDecorationNumber = Random.Range(0, _tileDecorationSpritesArray.Length);
+
             _tileDecorationRenderer.sprite = _tileDecorationSpritesArray[_tileDecorationNumber];
-            _tileSpriteHighlitRenderer.sprite = _tileDecorationSeenSpritesArray[_tileDecorationNumber];
+            _tileDecorationHighlitRenderer.sprite = _tileDecorationSeenSpritesArray[_tileDecorationNumber];
         }
         else
         {
             _hasDecoration = false;
+
             _tileDecorationRenderer.gameObject.SetActive(false);
+            _tileDecorationHighlitRenderer.gameObject.SetActive(false);
         }
 
         _tilesSpriteNumber = Random.Range(0, _tileSpritesArray.Length);
+
         _tileSpriteRenderer.sprite = _tileSpritesArray[_tilesSpriteNumber];
         _tileSpriteHighlitRenderer.sprite = _tileSpriteSeenArray[_tilesSpriteNumber];
 
         _tileSpriteRenderer.sortingOrder = 10 - row;
+        _tileSpriteHighlitRenderer.sortingOrder = 10 - row;
 
         _isVisible = false; // just to be sure
 
     }
 
     public void IsBecomeVisible(bool newCheck) {
-        _newState = newCheck;
+        //
     }
 
     public void OnTileSeen()
@@ -60,22 +88,32 @@ public class MapTile : MonoBehaviour
             return;
         }
 
+        if (_hasDecoration) { _decorationTurnSeen.PlayFeedbacks(); }
+
         _turnVisible.PlayFeedbacks();
+
         _isVisible = true;
     }
 
-    public void OnTileNormal()
+    public void OnPlayerTurn(GameState newState)
     {
+        if (newState != GameState.PlayerTurn)
+        {
+            return;
+        }
+
         if (_isVisible == false)
         {
             return;
         }
 
         _turnNormal.PlayFeedbacks();
+        if (_hasDecoration) { _decorationTurnNormal.PlayFeedbacks(); }
+
         _isVisible = false;
     }
 
-    public MMF_Player GetSeenTrigger () 
+    public MMF_Player GetSeenTrigger ()
     {
         return _turnVisibleLogic;
     }
